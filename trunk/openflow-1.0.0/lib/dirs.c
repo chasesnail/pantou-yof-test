be746ac1341eb3411439576ffd24fc4e2a10a204
eming623@gmail.com <eming623@gmail.com@3bab1e90-2b66-bd91-ceec-86efd9cf0b15>
2014-03-24 22:53:54 +0000

    original version of pantou openflow

---
+++ trunk/openflow-1.0.0/lib/dirs.c
@@ -0,0 +1,4 @@
+/* -*- mode: c; buffer-read-only: t -*- */
+const char ofp_pkgdatadir[] = "/usr/share/openflow";
+const char ofp_rundir[] = "/var/run";
+const char ofp_logdir[] = "/var/log/openflow";
